9544ac32cb677ec970905899521f065b702f6fcb
Tengda Yan <yantd@qq.com>
2026-03-24 17:41:57 +0800

    fix another typo

--- server/potato/urls.py
+++ server/potato/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('potato/msg', views.msg_handler, name='msg_handler'),
-    path('Potato/view.html', views.view_page, name='view_page'),
+    path('potato/view.html', views.view_page, name='view_page'),
 ]
